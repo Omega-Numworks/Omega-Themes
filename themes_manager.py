c5a10002c094c2a981aed0b9132565ae3c70bfae
Quentin Guidée <quentin.guidee@gmail.com>
2019-11-26 17:44:14 +0100

    Theme manager

---
+++ themes_manager.py
@@ -0,0 +1,137 @@
+import sys
+import argparse
+import io
+import os
+import json
+import tkinter as tk
+from PIL import Image, ImageTk
+
+
+def get_data(theme):
+    json_file = open("themes/" + theme + ".json", "r")
+    data = json.load(json_file)
+    json_file.close()
+    
+    return data
+
+
+def write_palette_h(data):
+    file = open("../escher/include/escher/palette.h", "w")
+    file.write("#ifndef ESCHER_PALETTE_H\n")
+    file.write("#define ESCHER_PALETTE_H\n\n")
+    file.write("#include <kandinsky/color.h>\n\n")
+    file.write("class Palette {\n")
+    file.write("public:\n")
+    
+    print(data["colors"])
+
+    for key in data["colors"].keys():
+        file.write("  constexpr static KDColor " + key + " = KDColor::RGB24(0x" + data["colors"][key] + ");\n")
+
+    file.write("  constexpr static KDColor DataColor[] = {Red, Blue, Green, YellowDark, Magenta, Turquoise, Pink, Orange};\n")
+    file.write("  constexpr static KDColor DataColorLight[] = {RedLight, BlueLight, GreenLight, YellowLight};\n")
+    file.write("};\n\n")
+
+    file.write("#define ATOM_APP_USE_PALETTE\n")
+    file.write("class AtomPalette {\n")
+    file.write("public:\n")
+    file.write("  constexpr static KDColor Unknown = KDColor::RGB24(0xeeeeee);\n")
+    file.write("  constexpr static KDColor AlkaliMetal = KDColor::RGB24(0xffaa00);\n")
+    file.write("  constexpr static KDColor AlkaliEarthMetal = KDColor::RGB24(0xf6f200);\n")
+    file.write("  constexpr static KDColor Lanthanide = KDColor::RGB24(0xffaa8b);\n")
+    file.write("  constexpr static KDColor Actinide = KDColor::RGB24(0xdeaacd);\n")
+    file.write("  constexpr static KDColor TransitionMetal = KDColor::RGB24(0xde999c);\n")
+    file.write("  constexpr static KDColor PostTransitionMetal = KDColor::RGB24(0x9cbaac);\n")
+    file.write("  constexpr static KDColor Metalloid = KDColor::RGB24(0x52ce8b);\n")
+    file.write("  constexpr static KDColor Halogen = KDColor::RGB24(0x00debd);\n")
+    file.write("  constexpr static KDColor ReactiveNonmetal = KDColor::RGB24(0x00ee00);\n")
+    file.write("  constexpr static KDColor NobleGas = KDColor::RGB24(0x8baaff);\n")
+    file.write("  constexpr static KDColor TableLines = KDColor::RGB24(0x323532);\n")
+    file.write("  constexpr static KDColor AtomColor[] = {\n")
+    file.write("      Unknown, AlkaliMetal, AlkaliEarthMetal, Lanthanide, Actinide, TransitionMetal,\n")
+    file.write("      PostTransitionMetal, Metalloid, Halogen, ReactiveNonmetal, NobleGas\n")
+    file.write("  };\n")
+    file.write("};\n\n")
+    file.write("#endif\n")
+
+    file.close()
+
+
+# parser = argparse.ArgumentParser(description="Process the themes.")
+# parser.add_argument('--theme', help='the name of the theme')
+
+# args = parser.parse_args()
+# data = get_data(args.theme)
+
+# write_palette_h(data)
+
+def get_available_themes():
+    themes = []
+    for file in os.listdir("themes"):
+        filename = os.path.splitext(file)[0]
+        theme = filename, filename.replace("_", " ").capitalize()
+
+        themes.append(theme)
+
+    return themes
+
+
+themes = get_available_themes()
+
+
+# UI
+
+class Window:
+    def apply_theme(self, theme_index):
+        if theme_index != ():
+            filename = themes[theme_index[0]][0]
+            data = get_data(filename)
+            write_palette_h(data)
+            self.popup("Omega - Themes", "Theme applied successfully")
+        else:
+            self.popup("Omega - Themes", "Please select a theme first")
+
+
+    def create_ui(self):
+        master = tk.Tk()
+        master.title("Omega - Themes")
+        master.geometry("260x400")
+        master.resizable(width=False, height=False)
+
+        # Image
+        image = ImageTk.PhotoImage(Image.open("Omega.png").resize((200, 70)))
+        panel = tk.Label(master, image = image, height = 100)
+        panel.pack(fill=tk.X)
+
+        # Listbox
+        listbox = tk.Listbox(master, height=15)
+        listbox.pack(fill=tk.X)
+
+        for i in range(len(themes)):
+            listbox.insert(i, themes[i][1])
+        
+        # Button
+        patch_button = tk.Button(master, text="Apply", command=lambda: self.apply_theme(listbox.curselection()))
+        patch_button.pack(fill=tk.X)
+
+        self.window = master
+
+        master.mainloop()
+
+    
+    def popup(self, title, message):
+        popup = tk.Toplevel()
+        popup.title("Omega - Themes")
+        popup.geometry("400x60")
+        popup.resizable(width=False, height=False)
+
+        message = tk.Label(popup, text=message, anchor="w")
+        message.pack(fill=tk.X)
+
+        button = tk.Button(popup, text="Exit", command=lambda: sys.exit())
+        button.pack(side=tk.RIGHT)
+        
+        popup.mainloop()
+
+
+Window().create_ui()
